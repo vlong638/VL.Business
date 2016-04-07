c990222bebc9c5a421b836b51a0bc4173b1e2c24
vlong638 <vlong638@163.com>
2016-04-07 17:58:00 +0800

    新增项目VL.User.Core

---
+++ VL.User.Core/DomainEntities/TUser.cs
@@ -0,0 +1,14 @@
+using System.Runtime.Serialization;
+using System;
+using VL.Common.DAS.Objects;
+using VL.User.Enums;
+
+namespace VL.User.Entities
+{
+    public static class TUserEx
+    {
+        //public static void Sample(this TUser tUser)
+        //{
+        //}
+    }
+}
